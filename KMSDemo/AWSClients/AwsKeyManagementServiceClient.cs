436df748f66d4be134ec311e6aef2c359b63ca4e
Sara Honorato <shonorato@buyatab.com>
2021-02-18 10:36:03 -0800

    encrypt decrypt functions

---
+++ KMSDemo/AWSClients/AwsKeyManagementServiceClient.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.KeyManagementService;
+using Amazon.KeyManagementService.Model;
+using Amazon.Runtime;
+
+namespace KMSDemo.AwsClients
+{
+    public class AwsKeyManagementServiceClient
+    {
+        private readonly RegionEndpoint _region;
+        private readonly BasicAWSCredentials _credentials;
+
+        public AwsKeyManagementServiceClient(RegionEndpoint region, BasicAWSCredentials credentials)
+        {
+            _region = region;
+            _credentials = credentials;
+        }
+
+        public async Task<string> GetKeyAsync(string keyName)
+        {
+            try
+            {
+                var kmsClient = new AmazonKeyManagementServiceClient(_credentials, _region);
+
+                var response = await kmsClient.ListAliasesAsync(new ListAliasesRequest
+                {
+                    Limit = 1000
+                });
+
+                var keyList = response.Aliases;
+
+                var foundAlias = keyList.FirstOrDefault(r => r.AliasName == "alias/" + keyName);
+                if (foundAlias != null)
+                {
+                    return foundAlias.TargetKeyId;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return string.Empty;
+        }
+
+        public string Encrypt(string textToEncrypt, string keyId)
+        {
+            try
+            {
+                var kmsClient = new AmazonKeyManagementServiceClient(_credentials, _region);
+                var encryptRequest = new EncryptRequest
+                {
+                    KeyId = keyId
+                };
+
+                var textBytes = Encoding.UTF8.GetBytes(textToEncrypt);
+                encryptRequest.Plaintext = new System.IO.MemoryStream(textBytes, 0, textBytes.Length);
+                var response = kmsClient.EncryptAsync(encryptRequest);
+                return Convert.ToBase64String(response.Result.CiphertextBlob.ToArray());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public string Decrypt(string encryptedText)
+        {
+            try
+            {
+                var fromBase64Bytes = Convert.FromBase64String(encryptedText);
+                var kmsClient = new AmazonKeyManagementServiceClient(_credentials, _region);
+                var decryptRequest = new DecryptRequest
+                {
+                    CiphertextBlob = new System.IO.MemoryStream(fromBase64Bytes, 0, fromBase64Bytes.Length)
+                };
+                var response = kmsClient.DecryptAsync(decryptRequest);
+                if (response.Result == null)
+                {
+                    return string.Empty;
+                }
+                return Encoding.UTF8.GetString(response.Result.Plaintext.ToArray());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public string DecryptPassword(string encryptedText)
+        {
+            try
+            {
+                var fromBase64Bytes = Convert.FromBase64String(encryptedText);
+                var kmsClient = new AmazonKeyManagementServiceClient(_credentials, _region);
+                var decryptRequest = new DecryptRequest
+                {
+                    CiphertextBlob = new System.IO.MemoryStream(fromBase64Bytes, 0, fromBase64Bytes.Length)
+                };
+                var response = kmsClient.DecryptAsync(decryptRequest);
+                if (response.Result == null)
+                {
+                    return string.Empty;
+                }
+                return Encoding.UTF8.GetString(response.Result.Plaintext.ToArray());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
---
+++ KMSDemo/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Amazon;
+using Amazon.Runtime;
+using KMSDemo.AwsClients;
+
+namespace KMSDemo
+{
+    class Program
+    {
+        private static readonly RegionEndpoint Region = RegionEndpoint.USWest2;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Encrypting Order Number:");
+
+            string orderNumber = Console.ReadLine();
+
+            //New Encrypt
+            var encryptionKey = GetKeyByAlias("test_key_sara");
+            var encrypted = EncryptedText(orderNumber, encryptionKey);
+            Console.WriteLine($" Encrypted OrderNo {orderNumber} with KMS as {encrypted}");
+            Console.WriteLine("------------------------------------");
+
+            //New Decrypt
+            var decrypted = DecryptedText(encrypted);
+            Console.WriteLine($" Decrypted Order Number {encrypted} with KMS as {decrypted}");
+            Console.WriteLine("------------------------------------");
+
+            //End
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("TADAAA");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Get the key that we want to use in the encryption from KMS
+        /// </summary>
+        /// <param name="alias">The "key name"</param>
+        /// <returns>The key id</returns>
+        private static string GetKeyByAlias(string alias)
+        {
+            try
+            {
+                var credentials =
+                    new BasicAWSCredentials("acessKey", "secretKey");
+
+                var kmsClient = new AwsKeyManagementServiceClient(Region, credentials);
+                var key = kmsClient.GetKeyAsync(alias);
+                return key.Result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Encrypted data using a AWS Key
+        /// </summary>
+        /// <param name="textToEncrypt">The data to encrypt</param>
+        /// <param name="keyId">The key id</param>
+        /// <returns></returns>
+        private static string EncryptedText(string textToEncrypt, string keyId)
+        {
+            if (string.IsNullOrEmpty(textToEncrypt))
+            {
+                return string.Empty;
+            }
+
+            try
+            {
+                var credentials = new BasicAWSCredentials("acessKey", "secretKey");
+                var kmsClient = new AwsKeyManagementServiceClient(Region, credentials);
+                return kmsClient.Encrypt(textToEncrypt, keyId);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Decrypt data previously encrypted by a AWS Key
+        /// </summary>
+        /// <param name="encryptedText">The encrypted text</param>
+        /// <returns>The  decrypted text</returns>
+        private static string DecryptedText(string encryptedText)
+        {
+            if (string.IsNullOrEmpty(encryptedText))
+            {
+                return string.Empty;
+            }
+
+            try
+            {
+                var credentials = new BasicAWSCredentials("acessKey", "secretKey");
+                var kmsClient = new AwsKeyManagementServiceClient(Region, credentials);
+                return kmsClient.Decrypt(encryptedText);
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
